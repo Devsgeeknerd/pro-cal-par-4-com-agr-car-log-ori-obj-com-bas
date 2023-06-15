17ba492b64ecaddaa8884e8c48b7c53a8d5289b6
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-15 15:58:02 -0300

    adicionando conteudo

---
+++ FabricaCalculosMatematicos.java
@@ -0,0 +1,23 @@
+package br.com.devsgeeknerd.calculadora.fabricas;
+
+import br.com.devsgeeknerd.calculadora.classes.Adicao;
+import br.com.devsgeeknerd.calculadora.classes.Divisao;
+import br.com.devsgeeknerd.calculadora.classes.Multiplicacao;
+import br.com.devsgeeknerd.calculadora.classes.Subtracao;
+import br.com.devsgeeknerd.calculadora.classes.CalculadorMatematico;
+
+public class FabricaCalculosMatematicos {
+    public CalculadorMatematico criarCalculador(int numero1, int numero2, String calculo) {
+        if (calculo.equals("+")) {
+            return new Adicao(numero1, numero2);
+        } else if (calculo.equals("-")) {
+            return new Subtracao(numero1, numero2);
+        } else if (calculo.equals("*")) {
+            return new Multiplicacao(numero1, numero2);
+        } else if (calculo.equals("/")) {
+            return new Divisao(numero1, numero2);
+        } else {
+            return null;
+        }
+    }
+}
